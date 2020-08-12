9594ca532b76697a74e87402264a94940ee573e5
Edwina Palmer <hekamara@gmail.com>
2020-08-12 17:09:42 -0400

    Module assignment done

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -7,6 +7,12 @@ class ListNode:
         self.prev = prev
         self.value = value
         self.next = next
+
+    def delete(self):
+        if self.prev:
+                self.prev.next = self.next
+        if self.next:
+            self.next.prev = self.prev
             
 """
 Our doubly-linked list class. It holds references to 
@@ -27,7 +33,17 @@ class DoublyLinkedList:
     the old head node's previous pointer accordingly.
     """
     def add_to_head(self, value):
-        pass
+        new_node = ListNode(value, None, None)
+        self.length += 1
+        if not self.head and not self.tail:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head.prev = new_node
+            self.head = new_node
+     
+
         
     """
     Removes the List's current head node, making the
@@ -35,7 +51,10 @@ class DoublyLinkedList:
     Returns the value of the removed Node.
     """
     def remove_from_head(self):
-        pass
+        value = self.head.value
+        self.delete(self.head)
+        return value
+        
             
     """
     Wraps the given value in a ListNode and inserts it 
@@ -43,7 +62,15 @@ class DoublyLinkedList:
     the old tail node's next pointer accordingly.
     """
     def add_to_tail(self, value):
-        pass
+        new_node = ListNode(value)
+        self.length += 1
+        if not self.head and not self.tail:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.prev = self.tail
+            self.tail.next = new_node
+            self.tail = new_node
             
     """
     Removes the List's current tail node, making the 
@@ -51,32 +78,75 @@ class DoublyLinkedList:
     Returns the value of the removed Node.
     """
     def remove_from_tail(self):
-        pass
+        value = self.tail.value
+        self.delete(self.tail)
+        return value
             
     """
     Removes the input node from its current spot in the 
     List and inserts it as the new head node of the List.
     """
     def move_to_front(self, node):
-        pass
+        if node is self.head:
+            return 
+        self.add_to_head(node.value)
+        self.delete(node)
         
     """
     Removes the input node from its current spot in the 
     List and inserts it as the new tail node of the List.
     """
     def move_to_end(self, node):
-        pass
+       if node is self.tail:
+           return
+       self.add_to_tail(node.value)
+       self.delete(node)
+       
 
     """
     Deletes the input node from the List, preserving the 
     order of the other elements of the List.
     """
     def delete(self, node):
-        pass
+        # Planning
+        if self.head is None and self.tail is None:
+            return
+        # TODO: Do we need error checking if node not in list?
+       
+        # This is the only node
+        if node is self.head and node is self.tail:
+            self.head = None
+            self.tail = None
+        # It's the head
+        elif node is self.head:
+            self.head = node.next
+            node.delete()
+        # It's the tail
+        elif node is self.tail:
+            self.tail = node.prev
+            node.delete()
+        # It's in the middle
+        else:
+            node.delete()
+
+        self.length -= 1
 
     """
     Finds and returns the maximum value of all the nodes 
     in the List.
     """
     def get_max(self):
-        pass
+        # How to get max
+        if self.head is None and self.tail is None:
+            return 
+
+        max_val = self.head.value
+        current = self.head.next
+        # Loop through nodes
+        while current is not None:
+            # compare value in node to max found
+            if current.value > max_val:
+                max_val = current.value
+            current = current.next
+        # return max found
+        return max_val
